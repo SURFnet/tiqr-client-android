d9fa539ce6c7ddf1f8317ec754c3bd5d7097c966
Özcan Kaymak <ozcan@egeniq.com>
2020-04-14 10:08:25 +0200

    Add v1 parser

---
+++ data/src/main/java/org/tiqr/data/api/adapter/ChallengeResponseAdapter.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2010-2020 SURFnet bv
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of SURFnet bv nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.tiqr.data.api.adapter
+
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonReader
+import org.tiqr.data.model.AuthenticationResponse
+import org.tiqr.data.model.AuthenticationResponse.Code.*
+import org.tiqr.data.model.EnrollmentResponse
+import org.tiqr.data.model.EnrollmentResponse.Code.*
+import org.tiqr.data.util.extension.inLenient
+
+/**
+ * Custom Moshi adapter to parse [EnrollmentResponse] in ascii & json format
+ */
+@Deprecated("Protocol version 1 (ascii) is not supported anymore")
+class EnrollmentResponseAdapter {
+    @FromJson
+    fun fromJson(
+            reader: JsonReader,
+            delegate: JsonAdapter<EnrollmentResponse>
+    ): EnrollmentResponse? {
+        return reader.inLenient {
+            when (peek()) {
+                JsonReader.Token.STRING -> {
+                    // v1 response format (ascii)
+                    EnrollmentResponse(code = when (nextString()) {
+                        "OK" -> { ENROLL_RESULT_SUCCESS }
+                        else -> { ENROLL_RESULT_INVALID_RESPONSE }
+                    })
+                }
+                JsonReader.Token.BEGIN_OBJECT -> {
+                    // v2 response format (json)
+                    delegate.fromJson(this)
+                }
+                else -> {
+                    // unknown response format
+                    null
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Custom Moshi adapter to parse [AuthenticationResponse] in ascii & json format
+ */
+@Deprecated("Protocol version 1 (ascii) is not supported anymore")
+class AuthenticationResponseAdapter {
+    @FromJson
+    fun fromJson(
+            reader: JsonReader,
+            delegate: JsonAdapter<AuthenticationResponse>
+    ): AuthenticationResponse? {
+        return reader.inLenient {
+            when (peek()) {
+                JsonReader.Token.STRING -> {
+                    // v1 response format (ascii)
+                    with(nextString()) {
+                        when {
+                            this == "OK" -> AuthenticationResponse(code = AUTH_RESULT_SUCCESS)
+                            this == "ACCOUNT_BLOCKED" -> AuthenticationResponse(code = AUTH_RESULT_ACCOUNT_BLOCKED)
+                            this == "INVALID_CHALLENGE" -> AuthenticationResponse(code = AUTH_RESULT_INVALID_CHALLENGE)
+                            this == "INVALID_REQUEST" -> AuthenticationResponse(code = AUTH_RESULT_INVALID_REQUEST)
+                            this == "INVALID_USERID" -> AuthenticationResponse(code = AUTH_RESULT_INVALID_USER_ID)
+                            startsWith("INVALID_RESPONSE:") -> {
+                                val remainingAttempts = substringAfter(':').toIntOrNull() ?: 0
+                                AuthenticationResponse(code = AUTH_RESULT_INVALID_RESPONSE, attemptsLeft = remainingAttempts)
+                            }
+                            else -> AuthenticationResponse(code = AUTH_RESULT_INVALID_RESPONSE)
+                        }
+                    }
+                }
+                JsonReader.Token.BEGIN_OBJECT -> {
+                    // v2 response format (json)
+                    delegate.fromJson(this)
+                }
+                else -> {
+                    // unknown response format
+                    null
+                }
+            }
+        }
+    }
+}
+
